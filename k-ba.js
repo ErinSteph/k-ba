8c5eb54d99ec0b7d8a17d48496aedd164d03b12f
Kinu <mail@kinuch.in>
2013-05-12 08:25:40 +0100

    0.0.0 (initial commit)
    If I put the right version of the script in the repo, it should
    currently have Image Hovers, Backlinks and animated thumbnails, with a
    few extra options.
    ReadMe in next commit.

---
+++ k-ba.js
@@ -0,0 +1,111 @@
+
+// - Your Settings
+	var ihov = 1; //Image Hovers. 1 = Show full image on thumbnail hover, 0 = off.
+		var ihovH = "70%"; //Max Height of Image Hovers.
+		var ihovW = "70%"; //Max Width of Image Hovers.
+	var bckl = 1; //Backlinks. 1 = Add links to a post's replies in it's header, 0 = off.
+		var shwr = 1; //Show "Replies: ". 1 = Add "Replies: " before a row of backlinks, 0 = off.
+	var agif = 1; //Animate .GIFs. 1 = Automatically animate .GIF thumbnails, 0 = off.
+
+// - Some globals, don't change
+	var d = document;var db = document.body;
+	
+// - Business		
+	if (ihov == 1){
+		var aElm = d.getElementsByTagName('a');
+		for (i=0; i<aElm.length; i++) {
+		if (aElm[i].href.match(/\.(jpg|jpeg|gif|png)$/)) {
+		var oImg = d.createElement('img');
+		oImg.setAttribute('src','');
+		oImg.setAttribute('id','hovx' + i);
+		oImg.style.maxHeight = ihovH;
+		oImg.style.maxWidth = ihovW;
+		db.appendChild(oImg,db.firstChild);
+		aElm[i].onmouseover = function() { oImg.setAttribute('src', this.href); }
+		aElm[i].onmouseout = function() { oImg.setAttribute('src',''); }
+		function killLnk() { oImg.setAttribute('src',''); }
+		aElm[i].addEventListener('click',killLnk, false);
+		var divName = 'hovx' + i;
+		var offX = 10;
+		var offY = -150;
+		function mouseX(evt) {
+		if (!evt) evt = window.event; 
+		if (evt.pageX) return evt.pageX; 
+		else if (evt.clientX)return evt.clientX + (d.documentElement.scrollLeft ?  d.documentElement.scrollLeft : db.scrollLeft); 
+		else return 0; }
+		function mouseY(evt) {
+		if (!evt) evt = window.event; 
+		if (evt.pageY) return evt.pageY; 
+		else if (evt.clientY)return evt.clientY + (d.documentElement.scrollTop ? d.documentElement.scrollTop : db.scrollTop); 
+		else return 0; }
+		function follow(evt) {
+		if (d.getElementById) {var obj = d.getElementById(divName).style; obj.visibility = 'visible'; obj.position = 'absolute';
+		obj.left = (parseInt(mouseX(evt))+offX) + 'px';
+		obj.top = (parseInt(mouseY(evt))+offY) + 'px';}}
+		d.onmousemove = follow;}}
+	}
+	
+	if (bckl == 1){
+		function updateBackLinks() {
+		var passValue = d.getElementsByName('postpassword')[0].value;
+		var nameValue = d.getElementsByName('name')[0].value;
+		var i;
+		var links = d.getElementsByTagName('a');
+		var linkslen = links.length;
+		for (i=0;i<linkslen;i++){
+		var linksclass = links[i].getAttribute('class');
+		var testref = links[i].parentNode.getAttribute('class');
+		if (linksclass != null && linksclass.indexOf('ref|') != -1 && (testref == undefined || testref != 'reflink')) {
+		var post = links[i].href.substr(links[i].href.indexOf('#') + 1);
+		var reply = links[i].parentNode.parentNode.parentNode.getElementsByTagName('a')[0].name;
+		var board = links[i].href.substring(0, links[i].href.indexOf('/res'));
+		board = board.substring(board.lastIndexOf('/')+1);
+		var tr = links[i].href.substring(links[i].href.lastIndexOf('/')+1, links[i].href.lastIndexOf('.'));					
+		addBackLinks(reply, post, tr, board);}}	
+		function addBackLinks (reply, post, tr, board) {
+		var postid = d.getElementById('reply' + post);
+		if (postid != undefined) {		
+		var postrefl = postid.querySelectorAll('span.reflink')[0];					
+		if (postrefl.innerHTML.indexOf(reply) == -1){	
+		if (shwr == 1){
+		if (postrefl.innerHTML.indexOf('<resps>Replies: </resps>') == -1){
+		postrefl.innerHTML += '<resps>Replies: </resps>'; }} 		
+		var e = d.createElement('a');
+		e.innerHTML='<u>>>' + reply + '</u>';
+		e.setAttribute('href','/' + board + '/res/' + tr + '.html#' + reply);
+		e.setAttribute('class','ref|' + board + '|' + tr + '|' + reply);
+		e.setAttribute('onclick','return highlight(\'' + reply + '\', true);');
+		postrefl.appendChild(e)
+		return linkslen++; }}
+		var tpostid = d.getElementById('thread' + post + board);
+		if (tpostid != undefined) {
+		var tpostrefl = tpostid.querySelectorAll('span.reflink')[0];
+		if (tpostrefl.innerHTML.indexOf(reply) == -1){	
+		if (shwr == 1){
+		if (tpostrefl.innerHTML.indexOf('<resps>Replies: </resps>') == -1){
+		tpostrefl.innerHTML += '<resps>Replies: </resps>'; }} 		
+		var te = d.createElement('a');
+		te.innerHTML='<u>>>' + reply + '</u>';
+		te.setAttribute('href','/' + board + '/res/' + tr + '.html#' + reply);
+		te.setAttribute('class','ref|' + board + '|' + tr + '|' + reply);
+		te.setAttribute('onclick','return highlight(\'' + reply + '\', true);');
+		tpostrefl.appendChild(te);
+		return linkslen++;}}}
+		d.getElementsByName('postpassword')[0].value = passValue;
+		d.getElementsByName('name')[0].value = nameValue;	
+		return 0;}
+		updateBackLinks();
+	}
+	
+	if (agif == 1){
+		function animaGifs() {
+		var thumbs = document.getElementsByTagName("img");
+		var num = thumbs.length;
+		for(i = 0; i < num; i++){
+		if(thumbs[i].className == "thumb"){
+		if(thumbs[i].src.match(".gif")){
+		thumbs[i].src = thumbs[i].src.replace("s.gif", ".gif");
+		thumbs[i].src = thumbs[i].src.replace("thumb", "src");}}}}
+		animaGifs();
+	}
+	
